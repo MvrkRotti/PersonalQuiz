acfe40ccc251a6a57566941f9c724c37bba3d706
Dan <danil.pestov@yandex.ru>
2023-09-04 22:48:22 +0300

    Create outlets

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -12,10 +12,33 @@ class QuestionsViewController: UIViewController {
     @IBOutlet var singleStackView: UIStackView!
     @IBOutlet var singleButtons: [UIButton]!
     
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
     
     private let questions = Question.getQuestions()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleButtonAnswerPressed() {
+    }
+    
+    @IBAction func rangedAnswerNuttonPressed() {
+    }
+    
+    
+    
 }
+
+
