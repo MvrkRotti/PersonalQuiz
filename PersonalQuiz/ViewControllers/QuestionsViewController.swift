3c24ba8f3de2c1442218e27d609de6fa53b2490d
Dan <danil.pestov@yandex.ru>
2023-09-05 22:05:34 +0300

    Set first question

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
     
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
     @IBOutlet var singleStackView: UIStackView!
     @IBOutlet var singleButtons: [UIButton]!
     
@@ -23,9 +26,14 @@ class QuestionsViewController: UIViewController {
     
     
     private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
     
     @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
@@ -41,4 +49,48 @@ class QuestionsViewController: UIViewController {
     
 }
 
+// MARK: - Private methods
+extension QuestionsViewController {
+    private func updateUI() {
+        //Hide stacks
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        //Get current question
+        let currentQuestion = questions[questionIndex]
+        
+        //Set current question for question label
+        questionLabel.text = currentQuestion.title
+        
+        //Calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        //Set progress for questionProgressView
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        // Set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: break
+        case .range: break
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView?.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+            
+        }
+    }
+    
+}
 
